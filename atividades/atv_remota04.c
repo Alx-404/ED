51feae1a221b2162a957fc860ebae15e93530318
Alx-404 <alexanderreis70@gmail.com>
2021-05-03 14:01:36 -0300

    solucao prof atv_remota04

--- atividades/atv_remota04.c
+++ atividades/atv_remota04.c
@@ -1,27 +1,44 @@
 #include <stdio.h>
 #include <string.h>
 
+char *Pesquisa(char *f,char c){
+	while(*f != '\0'){
+		if(*f == c){
+			return f;
+		}
+		f++;
+	}
+}
+
 char *PrimeiraVogal(char *s){
-	int i,j;
 	char vogais[] ="aeiouAEIOU";
-	
-	for( i=0; i<strlen(s); i++ ){
-		for ( j=0; j<strlen(vogais); j++){
-			if(s[i]==vogais[j]){
-				return &s[i];
-			}
-		}				
+	int i;
+	for (i=0; s[i] != '\0'; i++){
+		if(Pesquisa(vogais,s[i]) != NULL){ //podemos usar a funcao strchr(vogais,s[i]) da bliblioteca string.h
+			printf("Posicao da vogal na frase: %d\n",i);
+			return &s[i];
+		}
 	}
 	return NULL;
+
 }
 
 
 main(){
-	char palavra[10];
+	char frase[]="primeira vogal";
+	char *ptr;
+
+	ptr = PrimeiraVogal(frase);
 	
-	printf("Digite algo: ");
-	gets(palavra);
+	if(ptr != NULL){
+		printf("Vogal encontrada no endereco %p.",ptr);
+	}else{
+		printf("A frase informada nao possui vogal.");
+	}
 	
-	printf("endereco: %p ",PrimeiraVogal(palavra));
+
 	
 }
+
+
+
