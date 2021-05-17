080687e8752df8b3bf9e5e0f100b9f0268f5efe4
Alx-404 <alexanderreis70@gmail.com>
2021-05-17 09:59:07 -0300

    Revert "atv_remota07"
    This reverts commit e2db7cf7b1095fa1ef1cb76dffe5d1ea597e8c81.
    
    with '#' will be ignored, and an empty message aborts the commit.

--- ProjetoLista/listaE.h
+++
@@ -1,8 +0,0 @@
-struct celula{
-	int conteudo;
-	struct celula *prox;
-};
-
-typedef struct celula Celula;
-
-void inserir(int x,Celula *ini);
--- ProjetoLista/teste.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include "listaE.h"
-
-main(){
-	Celula *lista;
-	lista=malloc(sizeof(Celula));
-	lista->prox = NULL;
-	inserir(3,lista);
-}
